909d3329693c23f7e36ff69412206e43f99ad675
TharukaGamage19 <tharuugamage95@gmail.com>
2025-04-28 02:09:33 +0530

    fixed commit

--- 2425604/pos/main.py
+++
@@ -1,9 +0,0 @@
-from pos import POSSystem
-
-def main():
-    #entry
-    pos = POSSystem()
-    pos.main_menu()
-
-if __name__ == "__main__":
-    main()
--- main.py
+++
@@ -1,9 +0,0 @@
-from pos import POSSystem
-
-def main():
-    """Main entry point for the POS system"""
-    pos = POSSystem()
-    pos.main_menu()
-
-if __name__ == "__main__":
-    main()
